46310b431e1e708957093afe72bb9bc74334cf78
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2022-10-31 13:13:59 -0400

    asdf

---
+++ index.js
@@ -0,0 +1,18 @@
+let variable; //variable declaration
+variable = "Variable"; // variable definition
+let number = 7; //number data type
+let string = "Hello"; //strind data type
+let truthy = true; //boLeans
+let falsey = false; //boLeans
+let bigString = "Hello, I'm a bigger string"; //camel case
+
+var variable2 = "var"; //we dont do this
+let variable3; // we can define later OR REDEFINE
+variable = "nope";
+console.log(variable);
+const constant = "constant"; //cannot redefine
+
+const nullTest = null; //absence of value
+const undefinedTest;
+console.log(nullTest, undefinedTest);
+
